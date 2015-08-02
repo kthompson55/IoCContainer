a08e4fc670664e451fc7bcd5bb86753a5fe91003
Karl Thompson <karlthompsonnu@gmail.com>
2015-08-02 13:15:01 -0600

    Add IContainer interface
    -Add LifestyleType enum

---
+++ InversionOfControlProject/InversionOfControlProject/IContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InversionOfControlProject.Lifestyle;
+
+namespace InversionOfControlProject
+{
+    public interface IContainer
+    {
+        void Register(object resolver, object resolvee, LifestyleType lifeStyle);
+        void Resolve(object resolver);
+    }
+}
---
+++ InversionOfControlProject/InversionOfControlProject/Lifestyle/LifestyleType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InversionOfControlProject.Lifestyle
+{
+    public enum LifestyleTypeEnum
+    {
+        Transient,
+        Singleton
+    };
+    public class LifestyleType
+    {
+        public LifestyleTypeEnum Type { get; set; }
+    }
+}
